834ef65db6a169ad0cb6a106a02767f496d3d3f6
nkatajoshua <93196162+nkatajoshua@users.noreply.github.com>
2024-06-25 15:29:54 +0300

    Create input_output_functions.py

---
+++ Day 1/input_output_functions.py
@@ -0,0 +1,7 @@
+print("Greetings Dear User, this is an addition calculator")
+
+number1 = int(input("Enter your first number: "))
+number2 = int(input("Enter your second number: "))
+number3 = number1 + number2
+
+print(number3)
