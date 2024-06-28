f778b9689e36df9df2f1860fec6405e9720cf8bc
nkatajoshua <93196162+nkatajoshua@users.noreply.github.com>
2024-06-28 15:19:40 +0300

    Create datalist.py

---
+++ Day 3/datalist.py
@@ -0,0 +1,12 @@
+lst = [1,2,"Jo",5.6,"ucu",False]
+print(lst[2])
+
+lst2 = [3,5,[6,7,8]]
+print(lst2[2])
+print(lst2[2][1])
+
+lst3 = lst + lst2
+print(lst3)
+
+lst4 = [1,4] * 5 #number of times the list is to repeat
+print(lst4)
