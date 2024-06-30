5ff488407ca5de06ec5dee28b773a234ff5cf294
nkatajoshua <93196162+nkatajoshua@users.noreply.github.com>
2024-06-30 21:08:26 +0300

    list and tuple

--- Day 3/datalist.py
+++ Day 3/datalist.py
@@ -1,3 +1,4 @@
+#list Data Structure
 lst = [1,2,"Jo",5.6,"ucu",False]
 print(lst[2])
 
@@ -10,3 +11,31 @@ print(lst3)
 
 lst4 = [1,4] * 5 #number of times the list is to repeat
 print(lst4)
+
+for i in lst3:
+    print(i)
+
+print (1 in lst4) #does 1 exist in the list lst4
+
+#LIST METHODS
+lst8 = [5,3,10,30,24,67,21]
+lst8.append(90)
+lst8.append(78)
+print(lst8)
+
+lst8.remove(78)
+print(lst8)
+
+"""lst8.clear()
+print(lst8)"""
+
+lst8copy = lst8.copy()
+print(lst8copy)
+
+nindex = lst8.index(10)
+print(nindex)
+
+lst8copy.insert(0,70)#(position,the item you want)
+
+lst8copy.pop(5)
+print(lst8copy)
---
+++ Day 3/datatuple.py
@@ -0,0 +1,4 @@
+#tuple
+tun = (1,2,3,4,10,"tech",True)
+print(tun[-3])
+print(tun[3])
